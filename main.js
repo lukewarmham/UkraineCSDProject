c0604b6887af4a1042aa3b45f008b1a5a5d94eae
graigs-list <98970549+graigs-list@users.noreply.github.com>
2023-02-13 10:18:59 -0500

    replaced links with buttons

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello, world");
+let org = document.getElementById("orgs");
+let baseUrl;
+let url = document.location.toString().split('/');
+url.pop();
+baseUrl = url.join('/');
+
+function orgOnC() {
+    document.location = baseUrl + "/orgs/index.html";
+}
+
+function homeOnC() {
+    document.location = baseUrl + "/index.html";
+}
