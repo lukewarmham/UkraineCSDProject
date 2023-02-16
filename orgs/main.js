5721a06dab16f97980deacf18d801c8f8c2b34d1
graigs-list <98970549+graigs-list@users.noreply.github.com>
2023-02-16 09:32:55 -0500

    final

--- orgs/main.js
+++ orgs/main.js
@@ -11,3 +11,15 @@ function orgOnC() {
 function homeOnC() {
     document.location = baseUrl + "/index.html";
 }
+
+function orgone() {
+    document.location = "https://bank.gov.ua/en/news/all/natsionalniy-bank-vidkriv-spetsrahunok-dlya-zboru-koshtiv-na-potrebi-armiyi";
+}
+
+function orgtwo() {
+    document.location = "https://www.razomforukraine.org//donate/";
+}
+
+function orgthree() {
+    document.location = "https://helpukraine22.org/";
+}
